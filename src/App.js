37cb43d4e4167f33dc920711eb9cdd66a83a0df7
Masoom Belal <masumraza510@gmail.com>
2023-02-18 11:56:13 +0530

    This is text Formatter
    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
+import './App.css';
+import About from './components/About';
+import TextForm from './components/TextForm';
+import Navbar from './components/Navbar';
+import Alert from './components/Alert';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  // Link
+} from "react-router-dom"
 function App() {
+  const [mode, setMode] = useState('light'); // whether dark mode is enabled or not
+  const [alert,setAlert] = useState(null);
+
+  const showAlert=(message, type)=>{
+    setAlert({
+      msg:message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  }
+ let toggleMode =()=>{
+  if(mode==='light'){
+  setMode ('dark')
+   document.body.style.backgroundColor="#042743";
+   showAlert("Dark mode has enabled","success")
+  }else{
+    setMode('light');
+    document.body.style.backgroundColor="white";
+   showAlert("Light mode has enabled", "success")
+  }
+ }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>  
+
+    <Router>
+    <Navbar title="Textformatter" mode={mode} toggleMode={toggleMode}/>
+    <Alert alert={alert}/>
+    <div className="container my-3">
+     <Routes>
+      <Route exact path="/about" element={<About />} />
+      <Route exact path="/" element={ <TextForm showAlert={showAlert} heading="Enter the text to format" mode={mode}/>} />
+     </Routes>
+     </div>
+    </Router>
+    
+    </>
   );
 }
 
---
+++ src/components/Alert.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Alert(props) {
+    const capitalize = (word)=>{
+      const lower = word.toLowerCase();
+      return lower.charAt(0).toUpperCase() + lower.slice(1);
+    }
+  return (
+    props.alert && // it is used when it satifies then after this one will run other wise no like if the value is null it will not work 
+<div className= {`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+  <strong>{capitalize(props.alert.type)}</strong>: {props.alert.msg}
+  {/* <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> */}
+</div>
+  )
+}
+
+export default Alert
